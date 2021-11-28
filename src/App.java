68c77cb24b05a151c94e563e3a9b625c291afb33
PhilDusseldorf <86970605+PhilDusseldorf@users.noreply.github.com>
2021-11-28 16:20:38 +0100

    added comments

--- src/App.java
+++ src/App.java
@@ -2,17 +2,20 @@ import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) {
+        // make a window frame
         JFrame window = new JFrame();
         window.setTitle("2D Game");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
 
+        // make a Panel and add it to the window
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
 
         window.pack();
 
-        window.setLocationRelativeTo(null); // to center window
+        // position the window and make it visible
+        window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
 }
