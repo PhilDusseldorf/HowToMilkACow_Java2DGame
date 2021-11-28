27efd28667258598f9d71ea483f2095d4b9d8640
PhilDusseldorf <86970605+PhilDusseldorf@users.noreply.github.com>
2021-11-28 16:18:03 +0100

    set GamePanel

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import javax.swing.JFrame;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setTitle("2D Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null); // to center window
+        window.setVisible(true);
     }
 }
